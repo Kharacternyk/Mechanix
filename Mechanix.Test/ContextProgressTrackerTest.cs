e70619a0f2d31beb2c14c2f053b9ee37d4fc97f9
Kharacternyk <popcornunion@gmail.com>
2019-01-21 14:05:06 +0200

    Added checkpoints to ContextProgressTracker

--- Mechanix.Test/ContextProgressTrackerTest.cs
+++ Mechanix.Test/ContextProgressTrackerTest.cs
@@ -1,14 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
 
 using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Mechanix.Test
 {
     [TestClass]
-    public class ContextProgressTracker
+    public class ContextProgressTrackerTest
     {
         [TestMethod]
-        public void Test()
+        public void TestGoal()
         {
             var context = new PhysicalContext<int>(1, 1);
             context.AddEntity(0, new PointMass());
@@ -27,6 +29,24 @@ namespace Mechanix.Test
             AreEqual(1, tracker.Progress);
             IsFalse(tracker.IsActive);
             IsTrue(goal);
+        }
+
+        [TestMethod]
+        public void TestCheckPoints()
+        {
+            var context = new PhysicalContext<int>(1, 1);
+            context.AddEntity(0, new PointMass());
+
+            context.Tick();
+            ThrowsException<ArgumentOutOfRangeException>(() => new ContextProgressTracker<int>(context, 10, 0));
+
+            context.Tick();
+            var tracker = new ContextProgressTracker<int>(context, 10, 3, 5, 12);
+            var reached = 0;
+            tracker.OnCheckPoint += (c, _) => reached++;
+
+            context.Tick(12);
+            AreEqual(2, reached);
         }
     }
 }
--- Mechanix/ContextProgressTracker.cs
+++ Mechanix/ContextProgressTracker.cs
@@ -1,22 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Mechanix
 {
     /// <summary>
-    /// Represents <see cref="ContextObserver{TEntityKey}"/> 
+    /// Represents self-disposing <see cref="ContextObserver{TEntityKey}"/> 
     /// that tracks context current tick number 
     /// </summary>
     /// <typeparam name="TEntityKey"></typeparam>
     public sealed class ContextProgressTracker<TEntityKey> : ContextObserver<TEntityKey>
     {
+        readonly ulong[] _checkPoints;
+        int _currentCheckPointIndex;
+        public IEnumerable<ulong> CheckPoints => _checkPoints??Enumerable.Empty<ulong>();
+
         /// <summary>
         /// Goal tick
         /// </summary>
         public ulong Goal { get; }
+
         /// <summary>
-        /// Number in range [0; 1] that represent progress in reaching <see cref="this.Goal"/>
+        /// Number in range [0; 1] that represent progress in reaching <see cref="Goal"/>
         /// </summary>
         public double Progress => (LastObservedTick - ObservationBeginTick) / (Goal - ObservationBeginTick);
 
@@ -25,19 +30,43 @@ namespace Mechanix
         /// </summary>
         public event EventHandler<EventArgs> OnGoal;
 
+        /// <summary>
+        /// Raises when observable context reaches one of <see cref="CheckPoints"/>>
+        /// </summary>
+        public event EventHandler<EventArgs> OnCheckPoint;
+
         public ContextProgressTracker
         (
             PhysicalContext<TEntityKey> observableContext,
-            ulong goalTick
+            ulong goalTick,
+            params ulong[] checkPoints
         )
         : base(observableContext)
         {
+            if (checkPoints.Any(point => observableContext.Ticks > point))
+            {
+                throw new ArgumentOutOfRangeException
+                (
+                    $"Can't create ContextProgressTracker with checkpoints {CheckPoints}" +
+                    $"because one or more of checkpoints are already less then observable" +
+                    $"context current tick ({observableContext.Ticks})"
+                );
+            }
+            _checkPoints = checkPoints;
+            Array.Sort(_checkPoints);
+            _currentCheckPointIndex = 0;
             Goal = goalTick;
         }
 
         protected override void Observe()
         {
-            if (ObservableContext.Ticks == Goal)
+            var tick = ObservableContext.Ticks;
+            if (_checkPoints != null && _currentCheckPointIndex < _checkPoints.Length && tick == _checkPoints[_currentCheckPointIndex])
+            {
+                OnCheckPoint?.Invoke(this, EventArgs.Empty);
+                ++_currentCheckPointIndex;
+            }
+            if (tick == Goal)
             {
                 OnGoal?.Invoke(this, EventArgs.Empty);
                 Dispose();
