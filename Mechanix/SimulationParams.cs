2c8a9e6812bb01fca3f622effe3d442958175563
Kharacternyk <popcornunion@gmail.com>
2019-02-09 20:58:10 +0200

    Added docs for SimulationParams flags

--- Mechanix/SimulationParams.cs
+++ Mechanix/SimulationParams.cs
@@ -10,7 +10,19 @@ namespace Mechanix
     public enum SimulationParams
     {
         None =                0b_00000,
+        /// <summary>
+        /// Use parallel loop to processing next state of entities
+        /// <para></para>
+        /// NOTE: Exceptions during evaluation of force values will 
+        /// be wrapped into the <see cref="AggregateException"/> instance
+        /// </summary>
         ParallelEntities =    0b_00001,
+        /// <summary>
+        /// Use parallel loop to invoke <see cref="PhysicalContext{TEntityKey}.OnTick"/> subscribers
+        /// <para></para>
+        /// NOTE: Exceptions during invocation of subscribers will
+        /// be wrapped into the <see cref="AggregateException"/> instance
+        /// </summary>
         ParallelSubscribers = 0b_00010
     }
 }
