4f068c37690ad124f4701755034a9021b83af96c
Kharacternyk <popcornunion@gmail.com>
2019-02-15 20:36:53 +0200

    Optimized radial collision law

--- Mechanix/Laws/RadialCollisionLaw.cs
+++ Mechanix/Laws/RadialCollisionLaw.cs
@@ -16,12 +16,20 @@ namespace Mechanix.Laws
         /// </param>
         public static Force Eval(in PointMass objectEntity, in PointMass subjectEntity, double criticalDistance, double elasticityCoefficient)
         {
-            double dx2 = (objectEntity.X.Position - subjectEntity.X.Position) * (objectEntity.X.Position - subjectEntity.X.Position);
-            double dy2 = (objectEntity.Y.Position - subjectEntity.Y.Position) * (objectEntity.Y.Position - subjectEntity.Y.Position);
-            double dz2 = (objectEntity.Z.Position - subjectEntity.Z.Position) * (objectEntity.Z.Position - subjectEntity.Z.Position);
+            double dx2 = objectEntity.X.Position - subjectEntity.X.Position;
+            if (Math.Abs(dx2) > criticalDistance) return Force.Zero;
+            dx2 *= dx2;
+
+            double dy2 = objectEntity.Y.Position - subjectEntity.Y.Position;
+            if (Math.Abs(dy2) > criticalDistance) return Force.Zero;
+            dy2 *= dy2;
+
+            double dz2 = objectEntity.Z.Position - subjectEntity.Z.Position;
+            if (Math.Abs(dz2) > criticalDistance) return Force.Zero;
+            dz2 *= dz2;
 
             double distance = Math.Sqrt(dx2 + dy2 + dz2);
-            if (distance > criticalDistance) return new Force(0, 0, 0);
+            if (distance > criticalDistance) return Force.Zero;
 
             double deformation = criticalDistance - distance;
             double forceValue = deformation * elasticityCoefficient;
