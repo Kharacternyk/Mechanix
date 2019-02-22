7c7d61c5a42b50fd4340e466b5aea0f8ea0af25d
Kharacternyk <popcornunion@gmail.com>
2019-02-22 18:46:25 +0200

    Added ContextSharedValue class

---
+++ Benchmark/SharedValueBenchmark.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using Mechanix;
+
+namespace Benchmark
+{
+    public class SharedValueBenchmark
+    {
+        const double dt = 0.1;
+        readonly Random rand = new Random();
+
+        readonly System.Threading.Tasks.ParallelOptions _paralelOptions = new System.Threading.Tasks.ParallelOptions();
+
+        [Params(true, false)]
+        public bool ParallelEntities { get; set; }
+
+        PhysicalContext<int> context1000;
+        PhysicalContext<int> context1000Shared;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            context1000 = new PhysicalContext<int>(dt, 1000);
+            context1000Shared = new PhysicalContext<int>(dt, 1000);
+            var valShared = new ContextSharedValue<int, double>(context1000Shared, c => Math.Pow(Math.Log(c.Timer) + Math.Log10(c.Timer), Math.E));
+
+            for (int i = 0; i < 1000; ++i)
+            {
+                context1000.AddEntity
+                (
+                    i,
+                    new PointMass(rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), 1),
+                    getForce(i)
+                );
+                context1000Shared.AddEntity
+                (
+                    i,
+                    new PointMass(rand.NextDouble(), rand.NextDouble(), rand.NextDouble(), 1),
+                    getSharedForce(i)
+                );
+            }
+
+            Func<PhysicalContext<int>, Force> getForce(int i)
+            {
+                return c =>
+                {
+                    var val = Math.Pow(Math.Log(c.Timer) + Math.Log10(c.Timer), Math.E);
+                    return new Force(-c[i].X.Position * val, -c[i].Y.Position * val, -c[i].Z.Position * val);
+                };
+            }
+
+            Func<PhysicalContext<int>, Force> getSharedForce(int i)
+            {
+                return c => new Force(-c[i].X.Position * valShared.Value, -c[i].Y.Position * valShared.Value, -c[i].Z.Position * valShared.Value);
+            }
+        }
+
+        [Benchmark]
+        public void NonShared()
+        {
+            context1000.EntitiesParallelOptions = ParallelEntities ? _paralelOptions : null;
+            context1000.Tick(1);
+        }
+
+        [Benchmark]
+        public void Shared()
+        {
+            context1000Shared.EntitiesParallelOptions = ParallelEntities ? _paralelOptions : null;
+            context1000Shared.Tick(1);
+        }
+    }
+}
---
+++ Mechanix/ContextSharedValue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Mechanix
+{
+    /// <summary>
+    /// Represents cacheable, lazy-evaluated value, that depends on <see cref="Context"/> state
+    /// </summary>
+    public class ContextSharedValue<TEntityKey, TValue>
+    {
+        TValue _value;
+        ulong _tick;
+        readonly Func<PhysicalContext<TEntityKey>, TValue> _valueFunc;
+
+        readonly object _lockObject = new object();
+
+        public PhysicalContext<TEntityKey> Context { get; }
+        public TValue Value
+        {
+            get
+            {
+                if (Context.Ticks == _tick) return _value;
+
+                lock (_lockObject)
+                {
+                    _value = _valueFunc(Context);
+                    _tick = Context.Ticks;
+                }
+                return _value;
+            }
+        }
+
+        public ContextSharedValue
+        (
+            PhysicalContext<TEntityKey> context, 
+            Func<PhysicalContext<TEntityKey>, TValue> valueFunc
+        )
+        {
+            Context = context;
+            _valueFunc = valueFunc;
+            _value = _valueFunc(context);
+            _tick = context.Ticks;
+        }
+    }
+}
